ae2297ef3a2a3e0c76b31ff7a1cc3303832efec4
Jia Ming <jlim0176@student.monash.edu>
2022-05-06 01:56:11 +0800

    cuba perbaiki perwarnaan hamzah pada papan ketik

--- src/lib/statuses.ts
+++ src/lib/statuses.ts
@@ -44,7 +44,7 @@ export const getGuessStatuses = (
 
   // handle all correct cases first
   splitGuess.forEach((letter, i) => {
-    if (letter === splitSolution[i]) {
+    if (letter === splitSolution[i] || (letter==='ء'&&splitSolution[i]==='ٴ')) {
       statuses[i] = 'correct'
       solutionCharsTaken[i] = true
       return
