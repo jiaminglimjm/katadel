ddd7a8a1729ec4963842dbd38dee44bd91963a9c
Jia Ming <jlim0176@student.monash.edu>
2022-05-06 02:36:54 +0800

    penambahbaikan lagi

--- src/lib/statuses.ts
+++ src/lib/statuses.ts
@@ -44,7 +44,7 @@ export const getGuessStatuses = (
 
   // handle all correct cases first
   splitGuess.forEach((letter, i) => {
-    if (letter === splitSolution[i] || (letter==='ٴ'&&splitSolution[i]==='ء')) {
+    if (letter === splitSolution[i]) {
       statuses[i] = 'correct'
       solutionCharsTaken[i] = true
       return
